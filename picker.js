5b2fc8b97bee8a163f85350a9b1ac8d6d11005a8
ducky11423 <joshuanits@gmail.com>
2016-04-16 20:26:03 +0930

    Added more names and phrases

--- picker.js
+++ picker.js
@@ -1,6 +1,6 @@
-var names=["Shenanigans", "Stigin", "Cheese", "Mat", "The Duckmiester", "Gmate", "Blind", "Jaymo", "Kyle", "Latu", "Kuro", "Kinky", "ZooZoo", "Geoffery", "Vsystis", "Alpha", "Smallo", "Dislike", "Burtmin", "Duck", "Taven", "Freddie", "Prohax", "Saxton", "Cheater", "Giga", "Raime", "Grim", "Red", "Ceuloia", "Mansa", "Jibbyril", "Ham Peas", "Loopini", "Daxter"];
-var nouns=["Jew", "Social Justice Worker", "twat", "faggot", "furry", "shit stain", "fuckass", "cunt", "power hungry whore"];
-var adjectives=["queer", "butthurt", "annoying", "inbred", "gay", "salty", "fucked", "weird"];
+var names=["Shenanigans", "Stigin", "Cheese", "Mat", "The Duckmiester", "Gmate", "Blind", "Jaymo", "Kyle", "Latu", "Kuro", "Kinky", "ZooZoo", "Geoffery", "Vsytis", "Alpha", "Smallo", "Dislike", "Burtmin", "Duck", "Taven", "Freddie", "Prohax", "Saxton", "Cheater", "Giga", "Raime", "Grim", "Red", "Ceuloia", "Mansa", "Jibbyril", "Ham Peas", "Loopini", "Daxter", "Paul", "Micro", "Gundislav", "Fortunekid"];
+var nouns=["Jew", "Social Justice Worker", "twat", "faggot", "furry", "shit stain", "fuckass", "cunt", "power hungry whore", "sick cunt", "12 year old", "fucktard", "lad", "bag of dicks", "legit hacker"];
+var adjectives=["queer", "butthurt", "annoying", "inbred", "gay", "salty", "fucked", "weird", "getting banned", "literally worse than hitler", "3phie reincarnated"];
 
 function PickCombination(){
     var name = names[Math.floor(Math.random()*names.length)]
