a114c0eaf84a608854e66b4c7739e3fec4d10ab8
ducky11423 <joshuanits@gmail.com>
2016-04-17 10:57:59 +0930

    Forced stigin to be a wang

--- picker.js
+++ picker.js
@@ -1,5 +1,5 @@
 var names=["Alpha","Blind","Burtmin","Ceuloia","Cheater","Cheese","Church", "Dantenate", "Daxter","Dislike","Duck","Fortunekid","Freddie","Frog","Geoffery","Giga","Gmate","Grim","Gum", "Gundislav","Ham Peas","Jaymo","Jibbyril","Kinky","Kuro","Kyle","Latu","Loopini","Mansa","Mat","Meyers","Micro","Paul","Prohax","Raime","Red","Saxton","Shenanigans","Smallo","Stigin","Taven","The Duckmiester","Vystis", "Zettman", "Zeus", "ZooZoo"];
-var nouns=["Jew", "Social Justice Warrior", "twat", "faggot", "furry", "shit stain", "fuckass", "cunt", "power hungry whore", "sick cunt", "12 year old", "fucktard", "lad", "bag of dicks", "legit hacker", "rep whore", "wang", "wanker"];
+var nouns=["Jew", "Social Justice Warrior", "twat", "faggot", "furry", "shit stain", "fuckass", "cunt", "power hungry whore", "sick cunt", "12 year old", "fucktard", "lad", "bag of dicks", "legit hacker", "rep whore", "wang", "wanker", "Weeaboo"];
 var adjectives=["queer", "butthurt", "annoying", "inbred", "gay", "salty", "fucked", "weird", "getting banned", "literally worse than hitler", "3phie reincarnated"];
 var likes=["butt sex", "anal", "sucking up to the higher ups", "sucking Stigin off", "winding all the 12 year olds up"];
 
@@ -20,18 +20,23 @@ function PickCombination(name){
     
     
     if (rand == 0) {
-        var word = nouns[Math.floor(Math.random()*nouns.length)]
+        word = nouns[Math.floor(Math.random()*nouns.length)]
         deteminer = " is a "
     }
     
     else if (rand == 1){
-        var word = adjectives[Math.floor(Math.random()*adjectives.length)]
+        word = adjectives[Math.floor(Math.random()*adjectives.length)]
         deteminer = " is "
     }
     
     else if  (rand == 2){
-        var word = likes[Math.floor(Math.random()*likes.length)]
+        word = likes[Math.floor(Math.random()*likes.length)]
         deteminer = " likes "
+    }
+    
+    if (name == "Stigin"){
+        word = "wang"
+        deteminer = " is a "
     }
     
     display = document.getElementById("combination");
