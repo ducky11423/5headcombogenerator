6deb1bec579e1471f4193ca733b300f622628292
ducky11423 <joshuanits@gmail.com>
2016-04-17 11:15:31 +0930

    Added Soda + roll counter

--- picker.js
+++ picker.js
@@ -1,7 +1,9 @@
-var names=["Alpha","Blind","Burtmin","Ceuloia","Cheater","Cheese","Church", "Dantenate", "Daxter","Dislike","Duck","Fortunekid","Freddie","Frog","Geoffery","Giga","Gmate","Grim","Gum", "Gundislav","Ham Peas","Jaymo","Jibbyril","Kinky","Kuro","Kyle","Latu","Loopini","Mansa","Mat","Meyers","Micro","Paul","Prohax","Raime","Red","Saxton","Shenanigans","Smallo","Stigin","Taven","The Duckmiester","Vystis", "Zettman", "Zeus", "ZooZoo"];
+var names=["Alpha","Blind","Burtmin","Ceuloia","Cheater","Cheese","Church", "Dantenate", "Daxter","Dislike","Duck","Fortunekid","Freddie","Frog","Geoffery","Giga","Gmate","Grim","Gum", "Gundislav","Ham Peas","Jaymo","Jibbyril","Kinky","Kuro","Kyle","Latu","Loopini","Mansa","Mat","Meyers","Micro","Paul","Prohax","Raime","Red","Saxton","Shenanigans", "Smallo", "Soda", "Stigin","Taven","The Duckmiester","Vystis", "Zettman", "Zeus", "ZooZoo"];
 var nouns=["Jew", "Social Justice Warrior", "twat", "faggot", "furry", "shit stain", "fuckass", "cunt", "power hungry whore", "sick cunt", "12 year old", "fucktard", "lad", "bag of dicks", "legit hacker", "rep whore", "wang", "wanker", "Weeaboo"];
 var adjectives=["queer", "butthurt", "annoying", "inbred", "gay", "salty", "fucked", "weird", "getting banned", "literally worse than hitler", "3phie reincarnated"];
 var likes=["butt sex", "anal", "sucking up to the higher ups", "sucking Stigin off", "winding all the 12 year olds up"];
+
+var rolls = 0
 
 function PickCombination(){
     PickCombination("")
@@ -39,8 +41,13 @@ function PickCombination(name){
         deteminer = " is a "
     }
     
+     rolls = rolls + 1
+    
     display = document.getElementById("combination");
     display.innerHTML = name + deteminer + word + "!";
+    
+   rollP = document.getElementById("rolls");
+   rollP.innerHTML = "You have rolled: " + rolls + " times."
 }
 
 function PopulateList(){
